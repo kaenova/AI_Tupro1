91ac3a0dc6651ad9c909a29fdc0ecfffebb080e5
Kaenova Mahendra Auditama <kaenova@gmail.com>
2021-03-13 22:18:05 +0700

    nyoba-nyoba class

---
+++ test1.py
@@ -0,0 +1,17 @@
+import random
+
+class krom:
+    panjang = 5
+        
+    def __init__(self):
+        genotipe = []
+        for _ in range(self.panjang):
+            genotipe.append(random.randint(0,1))
+
+    def getGenotipe(self):
+        print(self.ge)
+    
+
+parent = krom()
+
+print(krom.)
